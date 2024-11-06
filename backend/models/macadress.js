a018337b202ffbc41fb551ed8fafe3984d8ebc23
SkyBlob12 <clement.andrieu12850@gmail.com>
2024-11-06 09:06:56 +0100

    Modif modeles port et macadress

--- backend/models/macadress.js
+++ backend/models/macadress.js
@@ -3,6 +3,11 @@ const sequelize = require('../database');
 
 // Définir le modèle MacAdresse
 const MacAdresse = sequelize.define('MacAdresse', {
+    id:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     adress: {
         type: DataTypes.STRING,
         allowNull: false,
--- backend/models/port.js
+++ backend/models/port.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../database');
+const MacAddress = require('./macadress');
 
 // Définir le modèle Port
 const Port = sequelize.define('Port', {
@@ -7,7 +8,18 @@ const Port = sequelize.define('Port', {
         type: DataTypes.INTEGER,
         allowNull: false,
         unique: true
+    },
+    macAddressId: {
+        type: DataTypes.INTEGER,
+        allowNull: false
     }
+});
+Port.belongsTo(MacAddress, {
+    foreignKey: {
+        name: 'macAddressId',
+        allowNull: false
+    },
+    onDelete: 'CASCADE'
 });
 
 module.exports = Port;

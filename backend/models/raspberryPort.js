dc253e3dd08b447eb726ffdca28a06e6ecaccafa
Nicolas-Delahaie <nico601.delahaie@gmail.com>
2024-11-07 13:33:34 +0100

    DB : ajout d'une PK dans raspberryport

--- backend/models/raspberryPort.js
+++ backend/models/raspberryPort.js
@@ -1,34 +1,41 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../database');
-const Raspberry = require('./raspberry');
-const ServerPort = require('./serverPort');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database");
+const Raspberry = require("./raspberry");
+const ServerPort = require("./serverPort");
 
 // Définir le modèle RaspberryPort
-const RaspberryPort = sequelize.define('RaspberryPort', {
-    port: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true
-    },
-    raspberryMac: {
-        type: DataTypes.STRING,
-        references: {
-            model: Raspberry,
-            key: 'mac'
+const RaspberryPort = sequelize.define(
+    "raspberryPort",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-    },
-    serverPort: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: ServerPort,
-            key: 'port'
+        port: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
         },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE'
-    }
-}, { tableName: 'RaspberryPort' });
-
+        raspberryMac: {
+            type: DataTypes.STRING,
+            references: {
+                model: Raspberry,
+                key: "mac",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+        },
+        serverPort: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: ServerPort,
+                key: "port",
+            },
+            onUpdate: "CASCADE",
+            onDelete: "CASCADE",
+        },
+    },
+    { timestamps: false }
+);
 
 module.exports = RaspberryPort;
